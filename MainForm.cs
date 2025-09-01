f944bfc95e4037af8568792aa80863d96b0c0c37
祐樹 漆畑 <y-urushibata@webi.co.jp>
2025-09-01 15:09:42 +0900

    PJ新規作成

---
+++ MainForm.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TranslationLens
+{
+    public partial class MainForm : Form
+    {
+        private Panel panel;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            panel = new Panel();
+            panel.BackColor = Color.Magenta;
+            this.Controls.Add(panel);
+
+            this.TransparencyKey = Color.Magenta;
+            this.TopMost = true;
+
+            AdjustPanelBounds();
+            this.Resize += (s, ev) => AdjustPanelBounds();
+        }
+
+        private void AdjustPanelBounds()
+        {
+            int margin = 8; // 枠を残す幅
+            panel.Bounds = new Rectangle(
+                margin,
+                margin,
+                this.ClientSize.Width - margin * 2,
+                this.ClientSize.Height - margin * 2
+            );
+        }
+    }
+}
